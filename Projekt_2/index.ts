38304b97b38ee79fbe842a0e3175d92ed7da548e
Julia Linek <leeenek0@gmail.com>
2022-03-18 23:05:56 +0100

    New solution, class Note

--- Projekt_2/index.ts
+++ Projekt_2/index.ts
@@ -8,14 +8,23 @@ const app = express();
 
 app.use(express.json()); // praca z JSONem
 
-let id = 0;
+const currDate = new Date();
 
-type Note = {
+class Note{
+
+  id?: number = 0;
   title: string;
   content: string;
-  createDate?: string; // string - data w formacie ISO
+  createDate?: string;
   tags?: string[];
-  id?: number;
+
+  constructor(note: Note){ // Pakuje do obiektu - przykład z konwersatorium 
+    this.title = note.title;
+    this.content = note.content;
+    this.createDate = currDate.toISOString();
+    this.tags = note.tags;
+    this.id = note.id++;
+  }
 };
 
 const notes: Note[] = []; // globalne przechowywanie notatek
@@ -25,21 +34,11 @@ app.get('/note', function (req: Request, res: Response) {
   // const jsonNote = JSON.stringify(note);
 
   res.send('GET Hello World');
+  
 });
 
 app.post('/note', function (req: Request, res: Response) {
   // const title = req.body.title; -  nie muszę parsować JSONa
-  const currDate = new Date();
-  const note: Note = {
-    id: id++,
-    title: req.body.title,
-    content: req.body.content,
-    createDate: currDate.toISOString(),
-    tags: req.body.tags,
-  };
-
-  notes.push(note);
-
   if (res.status(200)) {
     console.log(req.body); // e.x. req.body.title
     res.send('POST Hello World');
