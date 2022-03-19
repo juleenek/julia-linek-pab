6a7e216d6961679c428093bb836f09febc7c4752
Julia Linek <leeenek0@gmail.com>
2022-03-19 13:10:55 +0100

    Endpoint POST /note, title and content check

--- Projekt_2/index.ts
+++ Projekt_2/index.ts
@@ -1,49 +1,64 @@
 import express from 'express';
 import { Request, Response } from 'express';
-
-// npx tsc Projekt_2/index.ts
-// node Projekt_2/index.js
+// import uniqid from 'uniqid';
 
 const app = express();
 
 app.use(express.json()); // praca z JSONem
 
+// Przyklad parsowania którego teraz nie muszę robić
+// const jsonNote = JSON.stringify(note);
+
 const currDate = new Date();
 
-class Note{
-
-  id?: number = 0;
+class Note {
   title: string;
   content: string;
   createDate?: string;
   tags?: string[];
+  id?: number;
 
-  constructor(note: Note){ // Pakuje do obiektu - przykład z konwersatorium 
+  constructor(note: Note) {
+    // Pakuje do obiektu - przykład z konwersatorium
     this.title = note.title;
     this.content = note.content;
-    this.createDate = currDate.toISOString();
+    this.createDate = note.createDate;
     this.tags = note.tags;
-    this.id = note.id++;
+    this.id = note.id;
   }
-};
+}
 
-const notes: Note[] = []; // globalne przechowywanie notatek
+// const isKeyExist = (req: Request) => {
+//   Object.keys(req.body).forEach(value => {
+//     if (!Note.hasOwnProperty(value)) console.log(value);
+//   });
+// };
+
+const notes: Note[] = [];
 
 app.get('/note', function (req: Request, res: Response) {
-  // Przyklad parsowania
-  // const jsonNote = JSON.stringify(note);
-
   res.send('GET Hello World');
-  
 });
 
 app.post('/note', function (req: Request, res: Response) {
-  // const title = req.body.title; -  nie muszę parsować JSONa
-  if (res.status(200)) {
-    console.log(req.body); // e.x. req.body.title
-    res.send('POST Hello World');
+  const note: Note = req.body; // nie muszę parsować na JSON
+  const title = note.title;
+  const content = note.content;
+
+  if (title === undefined) {
+    res.status(404).send({
+      error: 'Please, enter a title',
+    });
+  }
+  if (content === undefined) {
+    res.status(404).send({
+      error: 'Please, enter a title',
+    });
   } else {
-    res.status(404);
+    // It returns the number of milliseconds between 1 January 1970 00:00:00 UTC and the given date as the contents of the Date() constructor.
+    note.id = new Date().valueOf();
+    notes.push(note);
+    res.status(201).send(note);
   }
 });
 
