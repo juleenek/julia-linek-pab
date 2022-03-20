3a8d9b4cc1bbc68b8062e57c0e598732920b6c12
Julia Linek <leeenek0@gmail.com>
2022-03-20 14:49:24 +0100

    checkRequired function and update note PUT

--- Projekt_2/index.ts
+++ Projekt_2/index.ts
@@ -28,6 +28,15 @@ class Note {
   }
 }
 
+// That function check required fields
+const checkRequired = (toCheck: any, res: Response, message: string) => {
+  if (toCheck === undefined) {
+    res.status(404).send({
+      error: message,
+    });
+  }
+};
+
 // const isKeyExist = (req: Request) => {
 //   Object.keys(req.body).forEach(value => {
 //     if (!Note.hasOwnProperty(value)) console.log(value);
@@ -38,39 +47,41 @@ const notes: Note[] = [];
 
 app.get('/note/:id', function (req: Request, res: Response) {
   const { id } = req.params;
+  const note = notes.find((note) => {
+    return note.id === +id;
+  });
 
-  if (notes.some((note) => note.id === +id)) {
-    let note = notes.filter((note) => {
-      return note.id === +id;
-    });
-    res.send(note);
-  } else{
-    res.status(404).send({
-      error: `Note not found`,
-    });
-  }
+  checkRequired(note, res, 'Note not found')
+  res.status(200).send(note);
+  
 });
 
 app.post('/note', function (req: Request, res: Response) {
   const note: Note = req.body; // nie muszę parsować na JSON
-  const title = note.title;
-  const content = note.content;
 
-  if (title === undefined) {
-    res.status(404).send({
-      error: `Please, enter a title`,
-    });
-  }
-  if (content === undefined) {
-    res.status(404).send({
-      error: `Please, enter a title`,
-    });
-  } else {
-    // It returns the number of milliseconds between 1 January 1970 00:00:00 UTC and the given date as the contents of the Date() constructor.
-    note.id = new Date().valueOf();
-    notes.push(note);
-    res.status(201).send(note);
-  }
+  checkRequired(note.title, res, 'Please, enter a title');
+  checkRequired(note.content, res, 'Please, enter a content');
+
+  // It returns the number of milliseconds between 1 January 1970 00:00:00 UTC and the given date as the contents of the Date() constructor.
+  note.id = new Date().valueOf();
+  notes.push(note);
+  res.status(201).send(note);
+});
+
+app.put('/note/:id', function (req: Request, res: Response) {
+
+  let note: Note = req.body;
+  let noteBefore = notes.find(e => e.id === (+req.params.id)); 
+
+  checkRequired(note.title, res, 'Please, enter a title');
+  checkRequired(note.content, res, 'Please, enter a content');
+  checkRequired(noteBefore, res, 'Note not found');
+  
+  // https://javascript.info/object-copy
+  noteBefore = Object.assign(noteBefore, note);
+ 
+  res.status(201).send(noteBefore);
+
 });
 
 app.listen(3000);
