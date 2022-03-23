ec73027bb05033e9e6b1db808586e071f5651c70
Julia Linek <leeenek0@gmail.com>
2022-03-23 18:59:37 +0100

    Tag CRUD

--- Projekt_2/index.ts
+++ Projekt_2/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { Request, Response } from 'express';
-import  Note  from '../Projekt_2/Note'
-// import uniqid from 'uniqid';
+import Note from '../Projekt_2/Note';
+import Tag from '../Projekt_2/Tag';
 
 const app = express();
 
@@ -13,7 +13,12 @@ app.use(express.json()); // praca z JSONem
 const currDate = new Date();
 
 // That function check required fields
-const checkRequired = (toCheck: any, res: Response, message: string, errNum: number) => {
+const checkRequired = (
+  toCheck: any,
+  res: Response,
+  message: string,
+  errNum: number
+) => {
   if (toCheck === undefined) {
     res.status(errNum).send({
       error: message,
@@ -21,16 +26,22 @@ const checkRequired = (toCheck: any, res: Response, message: string, errNum: num
   }
 };
 
+/////////////////// CRUDE NOTE ///////////////////
+
 const notes: Note[] = [];
 
 app.get('/note/:id', function (req: Request, res: Response) {
-  const { id } = req.params;
+  const id = +req.params.id;
   const note = notes.find((note) => {
-    return note.id === +id;
+    return note.id === id;
   });
 
   checkRequired(note, res, 'Note not found', 404);
   res.status(200).send(note);
+});
+
+app.get('/notes', function (req: Request, res: Response) {
+  res.status(200).send(notes);
 });
 
 app.post('/note', function (req: Request, res: Response) {
@@ -47,7 +58,7 @@ app.post('/note', function (req: Request, res: Response) {
 
 app.put('/note/:id', function (req: Request, res: Response) {
   let note: Note = req.body;
-  let noteBefore = notes.find(e => e.id === +req.params.id);
+  let noteBefore = notes.find((e) => e.id === +req.params.id);
 
   checkRequired(note.title, res, 'Please, enter a title', 404);
   checkRequired(note.content, res, 'Please, enter a content', 404);
@@ -60,13 +71,80 @@ app.put('/note/:id', function (req: Request, res: Response) {
 });
 
 app.delete('/note/:id', function (req: Request, res: Response) {
-
-  const note = notes.find(e => e.id === (+req.params.id));
+  const note = notes.find((e) => e.id === +req.params.id);
   checkRequired(note, res, 'Note not found', 400);
 
-  notes.splice(notes.findIndex(note => note.id === (+req.params.id)), 1)
+  notes.splice(
+    notes.findIndex((note) => note.id === +req.params.id),
+    1
+  );
   res.status(204).send(note);
-  
+});
+
+///////////////////// CRUDE TAG /////////////////////
+
+const tags: Tag[] = [];
+
+app.get('/tag/:id', function (req: Request, res: Response) {
+  const id = +req.params.id;
+  const tag = tags.find((tag) => {
+    return tag.id === id;
+  });
+
+  checkRequired(tag, res, 'Note not found', 404);
+  res.status(200).send(tag);
+});
+
+app.get('/tags', function (req: Request, res: Response) {
+  res.status(200).send(tags);
+});
+
+app.post('/tag', function (req: Request, res: Response) {
+  const tag: Tag = req.body;
+
+  checkRequired(tag.name, res, 'Please, enter a tag name', 400);
+
+  tag.id = new Date().valueOf();
+  const name = (tag.name = tag.name.toLowerCase());
+
+  if (tags.some((tag) => tag.name === name)) {
+    res.status(404).send({
+      error: 'Tag already exist',
+    });
+  } else {
+    tags.push(tag);
+    res.status(201).send(tag);
+  }
+});
+
+app.put('/tag/:id', function (req: Request, res: Response) {
+  let tag: Tag = req.body;
+  let tagBefore = tags.find((e) => e.id === +req.params.id);
+
+  checkRequired(tag.name, res, 'Please, enter a title', 404);
+  checkRequired(tagBefore, res, 'Note not found', 404);
+
+  const name = (tag.name = tag.name.toLowerCase());
+
+  if (tags.some((tag) => tag.name === name)) {
+    res.status(404).send({
+      error: 'Tag already exist',
+    });
+  } else {
+    tagBefore = Object.assign(tagBefore, tag);
+    res.status(201).send(tagBefore);
+  }
+});
+
+app.delete('/tag/:id', function (req: Request, res: Response) {
+  const tag = tags.find((e) => e.id === +req.params.id);
+  checkRequired(tag, res, 'Note not found', 400);
+
+  tags.splice(
+    tags.findIndex((tag) => tag.id === +req.params.id),
+    1
+  );
+  res.status(204).send(tag);
 });
 
 app.listen(3000);
